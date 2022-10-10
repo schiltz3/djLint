7b188639f1ab3ec8b3584d3beedc893de2068d64
John Schiltz <johnschiltz2000@gmail.com>
2022-10-10 16:53:13 -0500

    bug is in  indent_html

--- src/djlint/reformat.py
+++ src/djlint/reformat.py
@@ -13,25 +13,19 @@ from .formatter.expand import expand_html
 from .formatter.indent import indent_html
 from .formatter.js import format_js
 from .settings import Config
-import logging
 
 
 def reformat_file(config: Config, this_file: Path) -> dict:
     """Reformat html file."""
-    logging.basicConfig(level="DEBUG", filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
     rawcode = this_file.read_text(encoding="utf8")
 
     compressed = compress_html(rawcode, config)
-    logging.debug(f"\nCOMPRESSED\n{compressed}")
 
     expanded = expand_html(compressed, config)
-    logging.debug(f"\nEXPANDED\n{expanded}")
 
     condensed = condense_html(expanded, config)
-    logging.debug(f"\nCONDENSED\n{condensed}")
 
     beautified_code = indent_html(condensed, config)
-    logging.debug(f"\nBEAUTIFIED\n{beautified_code}")
 
     if config.format_css:
         beautified_code = format_css(beautified_code, config)
